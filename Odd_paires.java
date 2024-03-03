511df8e9c3254daf0a833680b859bc84372a3fc3
Mohammad Ali Haidari <156159480+M-Ali313@users.noreply.github.com>
2024-03-03 12:22:27 +0430

    Add files via upload

---
+++ Odd_paires.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+ 
+public class Odd_paires {
+    public static void main(String ara[]){
+        Scanner scan=new Scanner(System.in);
+        int t=scan.nextInt();
+        while(t-->0){            // difficalty 1044      Odd Pries 
+            long n=scan.nextLong();
+        System.out.println((n*n)/2);
+    }
+    }
+}
