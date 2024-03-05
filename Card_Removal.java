d94b0f759b551529f586d319c68613f1dd476d3c
Mohammad Ali Haidari <156159480+M-Ali313@users.noreply.github.com>
2024-03-05 08:29:38 +0430

    Add files via upload

---
+++ Card_Removal.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+ 
+public class Card_Removal {
+    public static void main(String ara[]){
+         Scanner scan=new Scanner(System.in);
+	            int t=scan.nextInt();
+	       while(t-->0){
+	           int n=scan.nextInt();
+	           int arr[]=new int[n];
+	       for(int i=0;i<n;i++){
+	           arr[i]=scan.nextInt();
+	           
+	       }
+	       Arrays.sort(arr);
+                for(int i=0;i<n;i++)
+                                         
+                          System.out.println("");
+	       }
+               
+    }
+}
